37f5aeaab9548fe7e4b5a171ada69a1eea731042
Joshua Tyler <josh.tyler@btinternet.com>
2020-04-18 17:29:51 +0100

    Working with incrementing address

--- sw/sdram_test/sdram_test.cpp
+++ sw/sdram_test/sdram_test.cpp
@@ -5,13 +5,10 @@
 
 int main(void)
 {
-	// N.B. Currently WbUart.hpp only supports constant address mode
-	// So the data written/returned will be non-intuitive
 	WbUart<uint8_t,8> uart("/dev/ttyUSB0", 460800);
 	std::vector<uint8_t> wr_dat = {0xCA,0xFE,0xDE,0xAD,0xBE, 0xEF};
-	//std::vector<uint8_t> wr_dat = {0xDE, 0xEF};
-	uart.write(0,wr_dat.begin(), wr_dat.end());
-	VectorUtility::print(uart.read(0,wr_dat.size()));
+	uart.write(0, AddressMode::INCREMENT, wr_dat.begin(), wr_dat.end());
+	VectorUtility::print(uart.read(0, AddressMode::INCREMENT, wr_dat.size()));
 
 	return 0;
 }

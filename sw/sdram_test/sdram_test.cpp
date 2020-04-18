5e5ccaf2cfe565ef868447e882ee8db3cddd7822
Joshua Tyler <josh.tyler@btinternet.com>
2020-04-18 16:21:13 +0100

    SDRAM controller working in hardware

--- sw/sdram_test/sdram_test.cpp
+++ sw/sdram_test/sdram_test.cpp
@@ -1,6 +1,6 @@
 
 #include <vector>
-#include "utility.h"
+#include "VectorUtility.h"
 #include "WbUart.hpp"
 
 int main(void)
@@ -9,9 +9,9 @@ int main(void)
 	// So the data written/returned will be non-intuitive
 	WbUart<uint8_t,8> uart("/dev/ttyUSB0", 460800);
 	std::vector<uint8_t> wr_dat = {0xCA,0xFE,0xDE,0xAD,0xBE, 0xEF};
-	std::vector<uint8_t> wr_dat = {0x01};
+	//std::vector<uint8_t> wr_dat = {0xDE, 0xEF};
 	uart.write(0,wr_dat.begin(), wr_dat.end());
-	print_data(uart.read(0,wr_dat.size()));
+	VectorUtility::print(uart.read(0,wr_dat.size()));
 
 	return 0;
 }
